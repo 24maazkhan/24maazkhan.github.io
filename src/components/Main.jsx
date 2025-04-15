69a434d8076c5e0600871529a4e49da1ae535912
Maaz Khan <24maazkhan@gmail.com>
2025-04-14 18:38:41 -0600

    Updated Home Page and NavBar to Fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     <div id='main'>
         <img className='w-full h-screen object-cover object-center' src='./bkg-image.jpg'/>
         <div className='w-full h-screen absolute top-0 left-0 bg-white opacity-50'>
-        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center'>
             <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Hi, I'm Maaz!</h1>
             <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a
             <TypeAnimation
@@ -26,11 +26,14 @@ const Main = () => {
                 repeat={Infinity}
                 />
             </h2>
+            <p className='flex sm:text-2xl text-xl pt-4 text-gray-800 text-center'>
+                Currently studying Systems Design Engineering at the University of Waterloo.
+            </p>
             <div className='flex justify-between pt-6 max-w-[80px] w-full'>
-                <a href='https://www.linkedin.com/in/maazkhan4' className='cursor-pointer'>
+                <a href='https://www.linkedin.com/in/maazkhan4' className='cursor-pointer' target='_blank'>
                     <FaLinkedinIn size={25}/>
                 </a>
-                <a href='https://github.com/24maazkhan' className='cursor-pointer'>
+                <a href='https://github.com/24maazkhan' className='cursor-pointer' target='_blank'>
                     <FaGithub size={25}/>
                 </a>
             </div>
